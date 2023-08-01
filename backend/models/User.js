7195475788419ee7023c7c2dc41ec189f63dc98f
mpmcauliffe <mcauliffe99@gmail.com>
2023-08-01 00:23:26 -0400

    BACKEND: add models/User

---
+++ backend/models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    exercises: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Exercise',
+    }],
+    workouts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Workout',
+    }],
+})
+
+
+module.exports = mongoose.model('User', userSchema);
