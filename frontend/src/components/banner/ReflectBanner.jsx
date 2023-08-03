e21c59f58b287e5f567ed08ba4c50c845babc161
mpmcauliffe <mcauliffe99@gmail.com>
2023-08-03 01:53:00 -0400

    FRONTEND: add components/banner/1x & img

---
+++ frontend/src/components/banner/ReflectBanner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Reflection } from './BannerComp'
+
+
+export const ReflectBanner = () => {
+    return (
+        <Reflection 
+            alt='RACEMATE!'
+            src={require(`./RACEMATE_reflect.png`)} />
+    )
+}
