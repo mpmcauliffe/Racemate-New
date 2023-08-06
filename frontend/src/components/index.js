dbb0df4fee6c0f12754890671cb3bd7ee6b56cd5
mpmcauliffe <mcauliffe99@gmail.com>
2023-08-06 01:07:47 -0400

    FRONTEND: add components/ 1.js

---
+++ frontend/src/components/index.js
@@ -0,0 +1,22 @@
+export * from './accordion/Accordion'
+export * from './alert/Alert'
+export * from './banner/Banner'
+export * from './banner/ReflectBanner'
+// export * from './calendar/Calendar'
+export * from './footer/Footer'
+export * from './forms/FormComp'
+export * from './forms/auth/Login'
+export * from './forms/auth/Signup'
+export * from './forms/userInfo/UserInfo'
+export * from './GridStack/GridStack'
+export * from './loader/Loader'
+export * from './lock/Lock'
+export * from './modal/activeModal/ActiveModal'
+export * from './modal/basicModal/BasicModal'
+export * from './nav/Nav'
+export * from './opSwitch/OpSwitch'
+export * from './updateBanner/UpdateBanner'
+
+
+// ./src/components/index.js
+//        Cannot find file './calendar/Calendar' in './src/components'.
