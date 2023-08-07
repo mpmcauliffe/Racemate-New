002340557f7c002036b2940ec5296e1034873f2d
mpmcauliffe <mcauliffe99@gmail.com>
2023-08-07 01:08:01 -0400

    FRONTEND: add context/ 1js

---
+++ frontend/src/context/types.js
@@ -0,0 +1,7 @@
+
+// ALERT CONTEXT
+export const SET_ALERT          = 'SET_ALERT'           
+export const REMOVE_ALERT       = 'REMOVE_ALERT'
+export const CHECK_FIELDS       = 'CHECK_FIELDS'        // verifies form input
+export const MATCH_PASSWORDS    = 'MATCH_PASSWORDS'     // matches password and confirm passwo
+export const AUTHORIZE_USER     = 'AUTHORIZE_USER'
