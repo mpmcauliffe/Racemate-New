e1c22f5cee2df5fa3a8d18d8dd44a02d7844d196
mpmcauliffe <mcauliffe99@gmail.com>
2023-08-08 16:30:59 -0400

    FRONTEND: add graphql/ 2js

---
+++ frontend/src/graphql/index.js
@@ -0,0 +1,12 @@
+//import { Query, } from './auth/userResolver'
+
+export * from './app/appRequests'
+export * from './app/appResolver'
+export * from './app/appTypes'
+export * from './auth/userRequests'
+export * from './auth/userTypes'
+
+
+export const rootResolver = {
+    //...Query,
+}
---
+++ frontend/src/graphql/types.js
@@ -0,0 +1,27 @@
+/* These TYPES were created for useContext/useReducer or Redux.
+ * Due to the different patterns of Apollo Cache they MAY NOT be 
+ * in use anywhere in this application. */
+
+/* EXERCISES */
+export const GET_EXERCISES              = 'GET_EXERCISES'
+export const GET_SINGLE_EXERCISE        = 'GET_SINGLE_EXERCISE'
+export const SAVE_EXERCISE              = 'SAVE_EXERCISE'
+export const EDIT_EXERCISE              = 'EDIT_EXERCISE'
+export const DELETE_EXERCISE            = 'DELETE_EXERCISE'
+export const CLEAR_EXERCISE             = 'CLEAR_EXERCISE'
+export const FILTER_BY_TYPE             = 'FILTER_BY_TYPE'
+export const CLEAR_TYPE                 = 'CLEAR_TYPE'
+
+/* USERS */
+export const REGISTER_SUCCESS           = 'REGISTER_SUCCESS'
+export const REGISTER_FAIL              = 'REGISTER_FAIL'
+export const ATTACH_USER                = 'ATTACH_USER'
+export const AUTH_ERROR                 = 'AUTH_ERROR' 
+export const LOGIN_SUCCESS              = 'LOGIN_SUCCESS'
+export const LOGIN_FAIL                 = 'LOGIN_FAIL'
+export const LOGOUT                     = 'LOGOUT'
+export const GET_USER                   = 'GET_USER'
+export const UPDATE_USER                = 'UPDATE_USER'
+
+/* MISC */
+export const CLEAR_ERRORS               = 'CLEAR_ERRORS'
